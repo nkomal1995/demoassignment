54a9758478cfd4d6e6c9eeea7b326bdefd89d011
komal.pixel6 <komal@thinkoverit.com>
2022-09-25 20:39:37 +0530

    question3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms' 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proAssig';
+
+
+  productForm: FormGroup;  
+
+  constructor(private fb:FormBuilder) {  
+     
+    this.productForm = this.fb.group({  
+      name: '',  
+      // name: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+
+
+      quantities: this.fb.array([]) ,  
+    });  
+
+
+
+  }  
+
+
+  quantities() : FormArray {  
+    return this.productForm.get("quantities") as FormArray  
+  }  
+  newQuantity(): FormGroup {  
+    return this.fb.group({  
+      qty: '',  
+    
+    })  
+  }  
+  addnumber() {  
+    this.quantities().push(this.newQuantity());  
+  }  
+     
+  removeQuantity(i:number) {  
+    this.quantities().removeAt(i);  
+  }  
+  onSubmit() {  
+    console.log(this.productForm.value);  
+  }  
+
+
 }
